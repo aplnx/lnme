9d29d2565288c1d4644e9f36244320d4f8569a4f
aplnx <aplnx@hotmail.com>
2021-09-06 08:37:33 -0300

    Script for generating sample tickets.

---
+++ files/gen_tickets.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+echo "PROMO10134"> ./tickets/ticket1.txt
+echo "PROMO40257"> ./tickets/ticket2.txt
+echo "PROMO20342"> ./tickets/ticket3.txt
+echo "PROMO50442"> ./tickets/ticket4.txt
+echo "PROMO50542"> ./tickets/ticket5.txt
+echo "PROMO50642"> ./tickets/ticket6.txt
+echo "PROMO50742"> ./tickets/ticket7.txt
+echo "PROMO50842"> ./tickets/ticket8.txt
+echo "PROMO50942"> ./tickets/ticket9.txt
+echo "PROMO51042"> ./tickets/ticket10.txt
+echo "PROMO51142"> ./tickets/ticket11.txt
+echo "PROMO51242"> ./tickets/ticket12.txt
+echo "PROMO51342"> ./tickets/ticket13.txt
+echo "PROMO51442"> ./tickets/ticket14.txt
+echo "PROMO51542"> ./tickets/ticket15.txt
+echo "PROMO51642"> ./tickets/ticket16.txt
+echo "PROMO51742"> ./tickets/ticket17.txt
+echo "PROMO51842"> ./tickets/ticket18.txt
+echo "PROMO51942"> ./tickets/ticket19.txt
+echo "PROMO52042"> ./tickets/ticket20.txt
+
+
+
